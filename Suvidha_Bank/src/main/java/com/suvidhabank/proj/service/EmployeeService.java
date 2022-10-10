95d5af7442c1222c5c7a50d06b3429346869ade6
CONCERTO_L207 <CONCERTO_L207@host.docker.internal>
2022-10-10 17:18:08 +0530

    EmployeeService commit;

--- Suvidha_Bank/src/main/java/com/suvidhabank/proj/service/EmployeeService.java
+++ Suvidha_Bank/src/main/java/com/suvidhabank/proj/service/EmployeeService.java
@@ -42,58 +42,58 @@ public boolean searchEmployeeById(String employeeId) {
 			System.out.println("error");
 		}
 				
-//				
-//		
-//		if (this.searchEmployeeById(empId)) 
-//		{			
-//			
-//			List<Loan> loans=this.loanRepository.findByEmployeeEmployeeId(empId);
-//			System.out.println(loans);
-//			if(loans.size()>0) {
-//				for(Loan loan:loans) {
-//					Customer customer=loan.getCustomer();
-//					if(loan.getLoanAmount() > (10 * customer.getAnnualIncome())) {
-//						loan.setRemarks("Loan amount cannot be 10 times of annual income");
-//						loan.setApproved(false);
-//						this.loanRepository.save(loan);
-//						
-//					}
-//					
-//					else if(!customer.isIncomeTaxReturnAttached()) {
-//						loan.setRemarks( "Income proof not attached");
-//						loan.setApproved(false);
-//						this.loanRepository.save(loan);
-//						
-//					}
-//					else if(customer.getCustomerIdentity() == null) {
-//						loan.setRemarks( "Identity document not submitted");
-//						loan.setApproved(false);
-//						this.loanRepository.save(loan);
-//						
-//					}
-//					else if(loan.getCollateral() == null || loan.getCollateral().size() == 0) {
-//						loan.setRemarks( "No collateral submitted");
-//					   loan.setApproved(false);
-//						this.loanRepository.save(loan);
-//						
-//					}
-//					else
-//					{
-//						loan.setRemarks( "Approved");
-//						loan.setApproved(true);
-//						
-//						this.loanRepository.save(loan);
-//						
-//					}
-//					System.out.println(loans);
-//				}
-//			}
-//			else {
-//				throw new Exception("No loan proposals against this employee id");
-//			}
-//		}
-//		else
-//			throw new EntityNotFoundException("Employee does not exist");
+				
+		
+		if (this.searchEmployeeById(empId)) 
+		{			
+			
+			List<Loan> loans=this.loanRepository.findByEmployeeEmployeeId(empId);
+			System.out.println(loans);
+			if(loans.size()>0) {
+				for(Loan loan:loans) {
+					Customer customer=loan.getCustomer();
+					if(loan.getLoanAmount() > (10 * customer.getAnnualIncome())) {
+						loan.setRemarks("Loan amount cannot be 10 times of annual income");
+						loan.setApproved(false);
+						this.loanRepository.save(loan);
+						
+					}
+					
+					else if(!customer.isIncomeTaxReturnAttached()) {
+						loan.setRemarks( "Income proof not attached");
+						loan.setApproved(false);
+						this.loanRepository.save(loan);
+						
+					}
+					else if(customer.getCustomerIdentity() == null) {
+						loan.setRemarks( "Identity document not submitted");
+						loan.setApproved(false);
+						this.loanRepository.save(loan);
+						
+					}
+					else if(loan.getCollateral() == null || loan.getCollateral().size() == 0) {
+						loan.setRemarks( "No collateral submitted");
+					   loan.setApproved(false);
+						this.loanRepository.save(loan);
+						
+					}
+					else
+					{
+						loan.setRemarks( "Approved");
+						loan.setApproved(true);
+						
+						this.loanRepository.save(loan);
+						
+					}
+					System.out.println(loans);
+				}
+			}
+			else {
+				throw new Exception("No loan proposals against this employee id");
+			}
+		}
+		else
+			throw new EntityNotFoundException("Employee does not exist");
 				}
 	
 	
